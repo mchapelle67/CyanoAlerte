82a5ef429206b88a491c64a781dbb86bb18d1667
Manon <manon.chp68@gmail.com>
2025-10-30 11:58:19 +0100

    création de l'entité photo

---
+++ migrations/Version20251030105755.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251030105755 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, waterbody_id INT DEFAULT NULL, url VARCHAR(255) NOT NULL, alt VARCHAR(255) DEFAULT NULL, INDEX IDX_16DB4F89BB7052F2 (waterbody_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F89BB7052F2 FOREIGN KEY (waterbody_id) REFERENCES waterbody (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F89BB7052F2');
+        $this->addSql('DROP TABLE picture');
+    }
+}
---
+++ src/Entity/Picture.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PictureRepository::class)]
+class Picture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $alt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'pictures')]
+    private ?Waterbody $Waterbody = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getAlt(): ?string
+    {
+        return $this->alt;
+    }
+
+    public function setAlt(?string $alt): static
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    public function getWaterbody(): ?Waterbody
+    {
+        return $this->Waterbody;
+    }
+
+    public function setWaterbody(?Waterbody $Waterbody): static
+    {
+        $this->Waterbody = $Waterbody;
+
+        return $this;
+    }
+}
